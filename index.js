c0658f165f653d3269546dd8fafa9d8411cdcafa
Nguyễn Thị Hoài Thương <86669230+thuongnguyen11@users.noreply.github.com>
2024-11-11 22:02:38 +0700

    add carousel (#1)
    * squad2

--- index.js
+++ index.js
@@ -15,3 +15,29 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+const CarouselComponent = () => {
+  return (
+    <Carousel
+      showArrows={true}
+      infiniteLoop={true}
+      showThumbs={false}
+      showStatus={false}
+      autoPlay={true}
+      interval={6100}
+    >
+      <div>
+        <img src="image1.jpg" alt="slide 1" />
+      </div>
+      <div>
+        <img src="image2.jpg" alt="slide 2" />
+      </div>
+    </Carousel>
+  );
+};
+
+export default CarouselComponent;
