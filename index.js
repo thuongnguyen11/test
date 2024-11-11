49e4b445947779ab058f595aae99659b5b2aa4a6
hoaithuong2001sm@gmail.com <thuong.nguyen@arenacommerce.com>
2024-11-11 21:56:50 +0700

    squad2

--- index.js
+++ index.js
@@ -7,8 +7,37 @@ import './styles/index.css';
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <App></App>
+      <App></App>
+      <App></App>
+      <App></App>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+const CarouselComponent = () => {
+  return (
+    <Carousel
+      showArrows={true}
+      infiniteLoop={true}
+      showThumbs={false}
+      showStatus={false}
+      autoPlay={true}
+      interval={6100}
+    >
+      <div>
+        <img src="image1.jpg" alt="slide 1" />
+      </div>
+      <div>
+        <img src="image2.jpg" alt="slide 2" />
+      </div>
+    </Carousel>
+  );
+};
+
+export default CarouselComponent;
